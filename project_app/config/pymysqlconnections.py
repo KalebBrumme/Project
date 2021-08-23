24340ae11768c079eecea009a4b4a22d30f64255
Kaleb Brumme <kalebmbrumme@gmail.com>
2021-08-23 16:45:29 -0700

    monday work

--- project_app/config/pymysqlconnections.py
+++ project_app/config/pymysqlconnections.py
@@ -6,7 +6,7 @@ class MySQLConnection:
         # change the user and password as needed
         connection = pymysql.connect(host = 'localhost',
                                     user = 'root', 
-                                    password = 'rootroot', 
+                                    password = 'root', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
@@ -22,23 +22,24 @@ class MySQLConnection:
     
                 executable = cursor.execute(query, data)
                 if query.lower().find("insert") >= 0:
-                    # INSERT queries will return the ID NUMBER of the row inserted
+            # INSERT queries will return the ID NUMBER of the row inserted
                     self.connection.commit()
                     return cursor.lastrowid
                 elif query.lower().find("select") >= 0:
-                    # SELECT queries will return the data from the database as a LIST OF DICTIONARIES
+            # SELECT queries will return the data from the database as a LIST OF DICTIONARIES
                     result = cursor.fetchall()
                     return result
                 else:
-                    # UPDATE and DELETE queries will return nothing
+            # UPDATE and DELETE queries will return nothing
                     self.connection.commit()
             except Exception as e:
-                # if the query fails the method will return FALSE
+        # if the query fails the method will return FALSE
                 print("Something went wrong", e)
                 return False
             finally:
-                # close the connection
+        # close the connection
                 self.connection.close() 
 # connectToMySQL receives the database we're using and uses it to create an instance of MySQLConnection
 def connectToMySQL(db):
     return MySQLConnection(db)
+
