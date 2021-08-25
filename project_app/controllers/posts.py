5c4761424db5903ec4b31313003ae9c0e6513ffb
Kaleb Brumme <kalebmbrumme@gmail.com>
2021-08-25 12:03:45 -0700

    upload in progress

--- project_app/controllers/posts.py
+++ project_app/controllers/posts.py
@@ -1,14 +1,11 @@
 import os
+from uuid import uuid4
 from project_app import app
 from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 # from project_app.models.post import Post
 from project_app.models.user import User
-
-
-UPLOAD_FOLDER = '/upload_folder/images'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+from project_app.models.channel import Channel
 
 
 @app.route("/dashboard")
@@ -29,26 +26,35 @@ def upload():
     return render_template('upload.html')
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/uploads", methods = ['GET', 'POST'])
+
+
+def allowed_file(filename):
+    print(filename)
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in app.config['UPLOAD_EXTENSIONS']
+
+
+@app.route("/uploads", methods = ['POST'])
 def upload_file():
     if request.method == 'POST':
+        print(request.files)
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            return redirect('/upload')
         file = request.files['file']
         if file.filename == '':
             flash('No file selected')
-            return redirect(request.url)
+            return redirect('/upload')
+        print(file)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename = filename))
+            file.save(os.path.join(app.static_folder, f"{app.config['UPLOAD_PATH']}/{filename}"))
+            return redirect('/dashboard')
     return redirect('/dashboard')
+
 
 @app.route('/uploads/<filename>')
 def download_file(name):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], name)
+    return send_from_directory(app.config['UPLOAD_PATH'], name)
+
